ec30154e0c64c7f56b43955e148d312ec0d837ae
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-04-27 23:39:56 +0300

    setting view engine

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = 8000;
 const app = express();
 
+app.set("view engine", "ejs");
 app.listen(PORT || process.env.PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
