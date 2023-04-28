58bee088461a2aa2ec6cd6268546f5b8f5e9678c
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-04-28 23:45:05 +0300

    setting static files in the public folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = 8000;
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.listen(PORT || process.env.PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
