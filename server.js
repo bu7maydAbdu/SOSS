cd003fb4de4a666dbd0cd0e571b021d1084ee92a
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-04-30 23:52:59 +0300

    parsing incoming requests with json payloads

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(PORT || process.env.PORT, () => {
   console.log(`server is running on ${PORT}`);
