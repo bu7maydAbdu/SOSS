aa087058ce9d40fb71e6847495e138be362b35d8
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-05-20 11:52:39 +0300

    user data managing buggs

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -7,21 +7,29 @@ module.exports = {
     try {
       console.log(req.user);
       const lostPeople = await Lost.find();
-      const accountData1 = await ProfileData.findOne({
-        user: req.user._id,
-      }).lean();
-      console.log(accountData1);
+      if (req.user) {
+        const accountData1 = await ProfileData.findOne({
+          user: req.user._id,
+        }).lean();
+        console.log(accountData1);
+
+        res.render("index.ejs", {
+          lostPeople: lostPeople,
+          userData: req.user,
+          accountData: accountData1,
+        });
+      } else {
+        res.render("index.ejs", {
+          lostPeople: lostPeople,
+          userData: "false",
+          accountData: "false",
+        });
+      }
       // console.log(lostPeople[0].postedBy.toString());
       // console.log(req.user.id);
 
       // console.log(typeof req.user.id, typeof lostPeople[0].postedBy);
       // console.log(req.user.id === lostPeople[0].postedBy ? "true" : "false");
-
-      res.render("index.ejs", {
-        lostPeople: lostPeople,
-        userData: req.user,
-        accountData: accountData1,
-      });
     } catch (err) {
       console.log(err);
     }
