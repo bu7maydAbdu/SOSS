ef360cd1519fcebb5ddb811504986478fee74702
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-05-23 23:29:21 +0300

    changing keys

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -7,10 +7,5 @@ cloudinary.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-cloudinary.config({
-  cloud_name: "dtdgzfjpt",
-  api_key: "866399881797535",
-  api_secret: "9GZGaaG1C_K0UmXPgZibLjELRdE",
-});
 
 module.exports = cloudinary;
