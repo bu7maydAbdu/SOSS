0c76abef623f96fa357e8ec8f392f4abb174e791
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-05-23 23:08:12 +0300

    migrating cloudinary

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,14 +1,16 @@
-const cloudinary = require("cloudinary").v2
-
+const cloudinary = require("cloudinary").v2;
 
 require("dotenv").config({ path: "./config/.env" });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+cloudinary.config({
+  cloud_name: "dtdgzfjpt",
+  api_key: "866399881797535",
+  api_secret: "9GZGaaG1C_K0UmXPgZibLjELRdE",
+});
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET 
-  });
-
-
-  module.exports = cloudinary
+module.exports = cloudinary;
